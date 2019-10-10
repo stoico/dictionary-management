b8e9985f1eec3f36b4ce1f8234c41b66570a3dac
Carmine Di Martino <cardimart@gmail.com>
2019-10-10 21:59:30 +0200

    Clean up code and fix formatting.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,32 @@
 <template>
-  <a-layout id="components-layout-demo-top" class="layout">
+  <a-layout
+    id="components-layout-demo-top"
+    class="layout"
+  >
     <a-layout-header>
       <div style="width: 1024px; margin: auto;">
         <div class="logo" />
         <a-menu
           theme="dark"
           mode="horizontal"
-          :defaultSelectedKeys="['2']"
+          :default-selected-keys="['2']"
           :style="{ lineHeight: '64px' }"
         >
-          <a-menu-item key="1">Example 1</a-menu-item>
-          <a-menu-item key="2">Example 2</a-menu-item>
-          <a-menu-item key="3">Example 3</a-menu-item>
+          <a-menu-item key="1">
+            Example 1
+          </a-menu-item>
+          <a-menu-item key="2">
+            Example 2
+          </a-menu-item>
+          <a-menu-item key="3">
+            Example 3
+          </a-menu-item>
         </a-menu>
       </div>
     </a-layout-header>
     <a-layout-content
-      style="padding: 0 50px; min-height: 100vh; min-width: 1024px; margin: 0 auto; box-sizing: content-box;"
+      style="padding: 0 50px; min-height: 100vh; min-width: 1024px; margin: 0 auto;
+      box-sizing: content-box;"
     >
       <a-breadcrumb style="margin: 16px 0">
         <a-breadcrumb-item>Home</a-breadcrumb-item>
@@ -29,7 +39,9 @@
         <nuxt />
       </div>
     </a-layout-content>
-    <a-layout-footer style="text-align: center">Created by Carmine for OneDot, 2019 ©</a-layout-footer>
+    <a-layout-footer style="text-align: center">
+      Created by Carmine for OneDot, 2019 ©
+    </a-layout-footer>
   </a-layout>
 </template>
 
