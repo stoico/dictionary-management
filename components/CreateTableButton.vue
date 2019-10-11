0102328bf0edceeb3543bd4b3d228cee382c827c
Carmine Di Martino <cardimart@gmail.com>
2019-10-11 03:26:14 +0200

    Define CreateTableButton.

---
+++ components/CreateTableButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <a-divider />
+
+    <a-input
+      v-model="dictionaryName"
+      style="width: 220px;"
+      autosize="true"
+      size="large"
+      default-value="Name"
+      :maxlength="max"
+    />
+    <a-button
+      type="primary"
+      class="editable-add-btn"
+      size="large"
+      @click="addDictionary"
+    >
+      <a-icon type="plus-circle" />
+      Create new dictionary
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dictionaryName: 'New dictionary',
+      max: 26,
+    };
+  },
+  methods: {
+    handleAdd() {
+
+    },
+    addDictionary() {
+      this.$store.commit('addDictionary', this.dictionaryName);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
