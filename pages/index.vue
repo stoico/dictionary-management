53f2c9f68cfb8352244f7fcc65c1d211c4f3023c
Carmine Di Martino <cardimart@gmail.com>
2019-10-11 03:25:23 +0200

    Pass dictionary prop to Table.

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <Table />
-  </div>
+  <Table :data-set="dict" />
 </template>
 
 <script>
-import Table from "~/components/Table.vue";
+import { uuid } from 'vue-uuid';
+import Table from '../components/Table';
 
 export default {
   components: {
-    Table
-  }
+    Table,
+  },
+  data() {
+    return {
+      dict: [
+        {
+          domain: 'Stonegrey',
+          range: 'Dark Grey',
+          validity: { status: true, reason: '' },
+          key: uuid.v4(),
+        },
+        {
+          domain: 'Dark Grey',
+          range: 'Black',
+          validity: { status: true, reason: '' },
+          key: uuid.v4(),
+        },
+        {
+          domain: 'Acquamarine',
+          range: 'Blue',
+          validity: { status: true, reason: '' },
+          key: uuid.v4(),
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style>
+.center-flex {
+  display: flex;
+  justify-content: center;
+}
+</style>
