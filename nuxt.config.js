e6ef3c944500d0b45d020e5d24d18df281f0c1a8
Carmine Di Martino <cardimart@gmail.com>
2019-11-08 01:17:21 +0100

    Change pages' title from onedot to Dictionary Management System.

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Dictionary Management System',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
